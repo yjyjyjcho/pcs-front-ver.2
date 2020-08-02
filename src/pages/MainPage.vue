33ca83f80c4ce423bc5448584018609b3e7cc101
imseongtae <dev.haemil@gmail.com>
2020-08-03 00:52:53 +0900

    feat: Update MainPage component to include app intro image

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,7 +1,10 @@
 <template>
 	<div class="wrapper">
 		<h2>MainPage</h2>
-		MainPage 입니다. fff
+		<!-- Intro Image를 구분하기 위한 영역 -->
+		<div class="img-container">
+			<img src="@/assets/img/intro-img.png" alt="소개" />
+		</div>
 	</div>
 </template>
 
@@ -10,3 +13,15 @@ export default {
 	name: 'MainPage',
 };
 </script>
+
+<style lang="scss" scoped>
+.img-container {
+	margin: 40px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	img {
+		width: 80%;
+	}
+}
+</style>
