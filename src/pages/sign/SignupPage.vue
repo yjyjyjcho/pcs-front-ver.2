449bf5a56fb156c2bc4cefc92daab1c3f06b16af
imseongtae <dev.haemil@gmail.com>
2020-08-03 00:01:55 +0900

    feat: Add SignupPage component

---
+++ src/pages/sign/SignupPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h2>회원가입 페이지</h2>
+		<signup-form />
+	</div>
+</template>
+
+<script>
+import SignupForm from '@/components/sign/SignupForm.vue';
+
+export default {
+	name: 'SignupPage',
+	components: {
+		SignupForm,
+	},
+};
+</script>
+
+<style></style>
