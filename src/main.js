b85765c6311af49f1cd21ff736fb2f4a2fd764a2
imseongtae <dev.haemil@gmail.com>
2020-08-03 17:28:17 +0900

    feat: Correct Synchronization error that occur when App is initialized

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './routes';
+import store from './store';
+
+import Cookies from 'js-cookie';
 
 Vue.config.productionTip = false;
 
-new Vue({
-	router,
-	render: h => h(App),
-}).$mount('#app');
+const init = () => {
+	const savedToken = Cookies.get('accessToken');
+	if (savedToken) {
+		return store.dispatch('signinByToken', savedToken);
+	} else {
+		return Promise.resolve();
+	}
+};
+
+init().then(() => {
+	new Vue({
+		router,
+		store,
+		render: h => h(App),
+	}).$mount('#app');
+});
