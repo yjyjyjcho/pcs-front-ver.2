e27382c415f01b1ab9abc147f748d3ced0867ded
imseongtae <dev.haemil@gmail.com>
2020-08-03 17:37:25 +0900

    feat: Add mutations types for sign user

---
+++ src/store/mutations-types.js
@@ -0,0 +1,13 @@
+export const FETCH_POST_LIST = 'FETCH_POST_LIST';
+
+/**
+ * Auth
+ * 사용자 정보를 추가하는 변이 타입 추가
+ */
+//
+export const SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN';
+export const SET_MY_INFO = 'SET_MY_INFO';
+
+// 사용자 인증과 관련된 데이터를 제거하는 변이 타입 추가
+export const DESTROY_ACCESS_TOKEN = 'DESTROY_ACCESS_TOKEN';
+export const DESTROY_MY_INFO = 'DESTROY_MY_INFO';
