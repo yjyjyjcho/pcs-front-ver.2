e41cf0d36735047d3c7152eb5a2d4a79eaa9628c
imseongtae <dev.haemil@gmail.com>
2020-08-03 17:25:28 +0900

    feat: Add api instance

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const createInstance = () => {
-	return axios.create({
-		baseURL: process.env.VUE_APP_API_URL,
-	});
-};
+const instance = axios.create({
+	baseURL: process.env.VUE_APP_API_URL,
+});
 
-function createInstanceWith(url) {
-	return axios.create({
-		baseURL: `${process.env.VUE_APP_API_URL}${url}`,
-	});
-}
+export default instance;
 
-export const instance = createInstance();
-export const auth = createInstanceWith('auth');
+/**
+ * 결론: 하나의 axios 객체로 작업을 이어가야 함!(적어도 이번 프로젝트에서는)
+ * axios 객체를 리턴하는 형태로 코드를 진행하기 위해 여러 방향을 시도해봤지만
+ * axios 객체가 리턴하는 형태로 코드를 작성하면 매번 새로운 객체로 리턴이 되는 것인지 token으로 인한 인증이 되지 않음
+ * 그래서 이를 해결하기 위해 하나의 aixos 객체로 모두 작업
+ */
+
+// 인증이 필요없는 페이지는 다른 axios 객체를 사용해도 되는 것일까?
