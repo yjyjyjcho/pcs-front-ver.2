a3201d5629f005a5d5a4cf5bf2ee2cc3fc35e8f0
imseongtae <dev.haemil@gmail.com>
2020-08-03 17:15:59 +0900

    feat: Add SigninPage component

---
+++ src/pages/sign/SigninPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h2>로그인 페이지</h2>
+		<signin-form />
+	</div>
+</template>
+
+<script>
+import SigninForm from '@/components/sign/SigninForm.vue';
+
+export default {
+	name: 'SigninPage',
+	components: {
+		SigninForm,
+	},
+};
+</script>
+
+<style></style>
