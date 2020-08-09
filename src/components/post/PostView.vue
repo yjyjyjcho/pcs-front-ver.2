f04dc0b551dc2b50c807241ddc969fe9a39e2ca2
imseongtae <dev.haemil@gmail.com>
2020-08-09 21:23:43 +0900

    feat: Add PostView component

---
+++ src/components/post/PostView.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="post-view">
+		<div>
+			<h2>{{ post.title }}</h2>
+			<span>게시글 번호: {{ post.id }}</span>
+			<strong>{{ post.user.nickname }} {{ post.createdAt }}</strong>
+		</div>
+		<p>
+			{{ post.contents }}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'PostView',
+	props: {
+		post: {
+			type: Object,
+			required: true,
+			validator(post) {
+				const isValidPostId = typeof post.id === 'number';
+				const isValidTitle = !!post.title && post.title.length;
+				const isValidContents = post.contents && post.contents.length;
+				return isValidPostId && isValidTitle && isValidContents;
+			},
+		},
+	},
+};
+</script>
+
+<style></style>
