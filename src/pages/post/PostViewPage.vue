b3003007ce86dc4ed38ec2ec6704cf3b9a106893
imseongtae <dev.haemil@gmail.com>
2020-08-10 03:18:47 +0900

    feat: Update PostViewPage to include author check method

--- src/pages/post/PostViewPage.vue
+++ src/pages/post/PostViewPage.vue
@@ -3,19 +3,13 @@
 		<!-- post가 로딩 중일 경우 -->
 		<loading-spinner v-if="!post" />
 		<template v-else>
-			<post-view :post="post" />
-			<div class="btn-container">
-				<router-link class="btn md" :to="{ name: 'PostListPage' }"
-					>목록</router-link
-				>
-				<router-link
-					class="btn md"
-					:to="{ name: 'PostEditPage', params: { postId } }"
-					>수정</router-link
-				>
-				<button class="btn md">삭제</button>
-			</div>
+			<post-view :post="post" @delete="onDelete" :isMyPost="isMyPost" />
 		</template>
+		<div class="btn-container">
+			<router-link class="btn md" :to="{ name: 'PostListPage' }">
+				목록
+			</router-link>
+		</div>
 	</div>
 </template>
 
@@ -23,7 +17,7 @@
 import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import PostView from '@/components/post/PostView.vue';
 
-import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState, mapGetters } from 'vuex';
 
 export default {
 	name: 'PostViewPage',
@@ -35,15 +29,29 @@ export default {
 		this.fetchData();
 	},
 	computed: {
-		...mapState(['post']),
+		...mapState(['post', 'me']),
+		...mapGetters(['isAuthorized']),
+		isMyPost() {
+			return this.isAuthorized && this.me.id === this.post.user.id;
+		},
 	},
 	methods: {
-		...mapActions(['fetchPost']),
+		...mapActions(['fetchPost', 'deletePost']),
 		async fetchData() {
 			try {
 				// path 자원에 접근하는 대신 route에 params의 자원을 할당
 				// this.postId = this.$route.params.postId;
 				await this.fetchPost(this.postId);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		async onDelete(deleteId) {
+			try {
+				// 순서 보장을 위해 await
+				// 자식 컴포넌트에서 전달하는 값 deleteId를 인자로 사용
+				await this.deletePost(deleteId);
+				this.$router.back();
 			} catch (error) {
 				console.log(error);
 			}
