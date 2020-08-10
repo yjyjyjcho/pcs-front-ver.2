271cf78d94576a7aa88bb1d6d24b303dc660e02f
imseongtae <dev.haemil@gmail.com>
2020-08-11 05:09:59 +0900

    feat: Update PostListPage component to include pagination component

--- src/pages/post/PostListPage.vue
+++ src/pages/post/PostListPage.vue
@@ -4,16 +4,19 @@
 		<loading-spinner v-if="isLoading" />
 		<!-- loding template -->
 		<template v-else>
-			<post-list :posts="posts" />
+			<post-list :posts="paginatedPosts" />
 			<router-link class="btn md" :to="{ name: 'PostCreatePage' }">
 				글쓰기
 			</router-link>
+			<!-- pagination -->
 		</template>
+		<app-pagination :list-data="posts" @onPaginate="onPaginate" />
 	</div>
 </template>
 
 <script>
 import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+import AppPagination from '@/components/common/AppPagination.vue';
 import PostList from '@/components/post/PostList.vue';
 
 import { mapActions, mapState } from 'vuex';
@@ -23,26 +26,37 @@ export default {
 	components: {
 		PostList,
 		LoadingSpinner,
+		AppPagination,
 	},
 	data() {
 		return {
 			isLoading: false,
+			paginatedPosts: [], // PostList로 전달
 		};
 	},
-	created() {
-		this.fetchData();
+	async created() {
+		await this.fetchData();
 	},
-	computed: { ...mapState(['posts']) },
+	computed: {
+		...mapState(['posts']),
+	},
 	methods: {
 		...mapActions(['fetchPosts']),
 		async fetchData() {
 			try {
 				this.isLoading = true;
 				await this.fetchPosts();
+				// 초기값 설정을 위해 보여줄 개수만큼 paginatedPosts에 할당
+				this.paginatedPosts = this.posts.slice(0, 7);
 				this.isLoading = false;
 			} catch (error) {
 				console.log(error);
 			}
+		},
+		onPaginate(paginatedPosts) {
+			// console.log(paginatedPosts);
+			// app-pagination 컴포넌트에서 보내준 값을 반영
+			this.paginatedPosts = paginatedPosts;
 		},
 	},
 };
