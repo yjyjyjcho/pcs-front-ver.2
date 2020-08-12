56e0fba69ec026468fe95e7a348139ddca573302
imseongtae <dev.haemil@gmail.com>
2020-08-13 07:37:45 +0900

    refactor: Do refactoring
    - to use vuex
    - Divide components using PerformanceDetail component

---
+++ src/components/performance/PerformanceDetail.vue
@@ -0,0 +1,107 @@
+<template>
+	<section class="info-section">
+		<h2 class="prfm-title">
+			{{ performance.prfnm }}
+		</h2>
+		<div class="info-table">
+			<figure
+				class="item-image"
+				:style="{ backgroundImage: 'url(' + performance.poster + ')' }"
+			></figure>
+			<table>
+				<tr>
+					<td>기간</td>
+					<td>{{ performance.prfpdfrom }} ~ {{ performance.prfpdto }}</td>
+				</tr>
+				<tr>
+					<td>장소</td>
+					<td>{{ performance.fcltynm }}</td>
+				</tr>
+				<tr>
+					<td>출연진</td>
+					<td>{{ performance.prfcast }}</td>
+				</tr>
+				<tr>
+					<td>관람연령</td>
+					<td>{{ performance.prfage }}</td>
+				</tr>
+				<tr>
+					<td>기획제작사</td>
+					<td>{{ performance.entrpsnm }}</td>
+				</tr>
+				<tr>
+					<td>티켓가격</td>
+					<td>{{ performance.pcseguidance }}</td>
+				</tr>
+				<tr>
+					<td>장르</td>
+					<td>{{ performance.genrenm }}</td>
+				</tr>
+				<tr>
+					<td>공연시간</td>
+					<td>{{ performance.dtguidance }}</td>
+				</tr>
+				<tr>
+					<td>공연상태</td>
+					<td>{{ performance.prfstate }}</td>
+				</tr>
+			</table>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'PerformanceDetail',
+	props: {
+		performance: {
+			type: Object,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.info-section {
+	h2.prfm-title {
+		color: $primary-lighten-2;
+		margin: 10px 0;
+	}
+	.info-table {
+		display: flex;
+		height: 350px;
+		// TabMenu와 거리를 띄우기 위해 margin 설정
+		margin-bottom: 50px;
+		.item-image {
+			width: 300px;
+			padding-top: 10%;
+			background-color: lightgray;
+			background-repeat: no-repeat;
+			background-position: center;
+			background-size: cover;
+		}
+		table {
+			flex-grow: 1;
+			// width: 50%;
+			margin-left: 10px;
+
+			tr > td:first-child {
+				width: 100px;
+				padding: 10px 20px 10px 10px;
+				font-weight: bold;
+				border-bottom: 1px solid $gray-scale-2;
+				background-color: $gray-scale-1;
+
+				&:last-child {
+					border-bottom: none;
+					color: red;
+				}
+			}
+			tr > td:last-child {
+				padding: 10px 0px 10px 10px;
+			}
+		}
+	}
+}
+</style>
--- src/pages/performance/PerformanceDetailPage.vue
+++ src/pages/performance/PerformanceDetailPage.vue
@@ -3,55 +3,8 @@
 		<h2>공연 상세 페이지</h2>
 		<loading-spinner v-if="isLoading" />
 		<template v-else>
-			<section class="info-section">
-				<h2 class="prfm-title">
-					{{ prfData.prfnm }}
-				</h2>
-				<div class="info-table">
-					<figure
-						class="item-image"
-						:style="{ backgroundImage: 'url(' + prfData.poster + ')' }"
-					></figure>
-					<table>
-						<tr>
-							<td>기간</td>
-							<td>{{ prfData.prfpdfrom }} ~ {{ prfData.prfpdto }}</td>
-						</tr>
-						<tr>
-							<td>장소</td>
-							<td>{{ prfData.fcltynm }}</td>
-						</tr>
-						<tr>
-							<td>출연진</td>
-							<td>{{ prfData.prfcast }}</td>
-						</tr>
-						<tr>
-							<td>관람연령</td>
-							<td>{{ prfData.prfage }}</td>
-						</tr>
-						<tr>
-							<td>기획제작사</td>
-							<td>{{ prfData.entrpsnm }}</td>
-						</tr>
-						<tr>
-							<td>티켓가격</td>
-							<td>{{ prfData.pcseguidance }}</td>
-						</tr>
-						<tr>
-							<td>장르</td>
-							<td>{{ prfData.genrenm }}</td>
-						</tr>
-						<tr>
-							<td>공연시간</td>
-							<td>{{ prfData.dtguidance }}</td>
-						</tr>
-						<tr>
-							<td>공연상태</td>
-							<td>{{ prfData.prfstate }}</td>
-						</tr>
-					</table>
-				</div>
-			</section>
+			<!-- performance detail -->
+			<performance-detail :performance="performance" />
 			<!-- tabmenu -->
 			<app-tab
 				v-bind:tabs="tabs"
@@ -62,14 +15,18 @@
 				<!-- 이미지가 없거나 하나만 있을 경우 처리하는 로직을 만들어야 함 -->
 				<!-- 포스터 이미지가 한 장뿐인 경우 자료형을 배열로 교체 -->
 				<figure>
-					<img :src="prfData.poster" class="poster" :alt="prfData.prfnm" />
+					<img
+						:src="performance.poster"
+						class="poster"
+						:alt="performance.prfnm"
+					/>
 				</figure>
 			</section>
 			<section v-if="selectedTab === tabs[1]">
 				<!-- 지명, 주소 -->
-				<h3>{{ prfData.fcltynm }}</h3>
+				<h3>{{ performance.fcltynm }}</h3>
 				<p>
-					{{ prfData.adres }}
+					{{ performance.adres }}
 				</p>
 				<!-- 지도는 if로 그려야 함 -->
 				<app-map :centerData="center" />
@@ -86,40 +43,51 @@ import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import AppTab from '@/components/common/AppTab.vue';
 import AppMap from '@/components/common/AppMap.vue';
 
-import { getPerformanceDetail } from '@/api/performance';
+import PerformanceDetail from '@/components/performance/PerformanceDetail';
+
+// import { fetchPerformanceAPI } from '@/api/performance';
 import { getFacilityDetail } from '@/api/facility';
 
+import { mapState, mapActions } from 'vuex';
+
 export default {
+	name: 'PerformanceDetailPage',
 	components: {
 		LoadingSpinner,
 		AppTab,
 		AppMap,
+		PerformanceDetail,
+	},
+	props: {
+		mt20id: { required: true },
 	},
 	data() {
 		return {
 			isLoading: false,
-			mt20id: '',
 			tabs: ['공연상세', '공연시설', '연관정보'],
 			selectedTab: '',
-			prfData: [],
 			// daum map 속성
 			center: { lat: '', lng: '' }, // 지도의 중심 좌표
 		};
 	},
 	async created() {
-		await this.getPerformanceDetail();
+		console.log('퍼포먼스 아이디가 나간다!!', this.mt20id);
+		await this.fetchData();
 		this.selectedTab = this.tabs[0];
 	},
+	computed: {
+		...mapState(['performance']),
+	},
 	methods: {
-		async getPerformanceDetail() {
+		...mapActions(['fetchPerformance']),
+		async fetchData() {
 			this.isLoading = true;
-			this.mt20id = this.$route.params.mt20id;
-			const { data } = await getPerformanceDetail(this.mt20id);
-			const facilityData = await getFacilityDetail(data.mt10id);
+			// const { data } = await fetchPerformanceAPI(this.mt20id);
+			await this.fetchPerformance(this.mt20id);
+			console.log('state에 담기는 값은: ', this.performance);
+			const facilityData = await getFacilityDetail(this.performance.mt10id);
 			const { la, lo } = facilityData.data;
 			this.center = { lat: la, lng: lo };
-			this.prfData = data;
-			// console.log(data);
 			this.isLoading = false;
 		},
 		onClickTab(tab) {
@@ -130,49 +98,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.info-section {
-	h2.prfm-title {
-		color: $primary-lighten-2;
-		margin: 10px 0;
-	}
-}
-
-.info-table {
-	display: flex;
-	height: 350px;
-	// TabMenu와 거리를 띄우기 위해 margin 설정
-	margin-bottom: 50px;
-	.item-image {
-		width: 300px;
-		padding-top: 10%;
-		background-color: lightgray;
-		background-repeat: no-repeat;
-		background-position: center;
-		background-size: cover;
-	}
-	table {
-		flex-grow: 1;
-		// width: 50%;
-		margin-left: 10px;
-
-		tr > td:first-child {
-			width: 100px;
-			padding: 10px 20px 10px 10px;
-			font-weight: bold;
-			border-bottom: 1px solid $gray-scale-2;
-			background-color: $gray-scale-1;
-
-			&:last-child {
-				border-bottom: none;
-				color: red;
-			}
-		}
-		tr > td:last-child {
-			padding: 10px 0px 10px 10px;
-		}
-	}
-}
-
 // 공연 소개 이미지 가로 사이즈 100%로 설정
 .poster {
 	width: 100%;
