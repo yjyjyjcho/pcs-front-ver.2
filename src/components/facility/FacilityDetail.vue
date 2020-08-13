b528ef22fd18dfc05eba3b7722f872653457024a
imseongtae <dev.haemil@gmail.com>
2020-08-14 05:38:34 +0900

    refactor: Do refactoring
    - Use vuex for facility data
    - Divide components

---
+++ src/components/facility/FacilityDetail.vue
@@ -0,0 +1,86 @@
+<template>
+	<section class="info-section">
+		<h2 class="prfm-title">
+			{{ facility.fcltynm }}
+		</h2>
+		<div class="info-table">
+			<table>
+				<tr>
+					<td>장소</td>
+					<td>{{ facility.fcltynm }}</td>
+				</tr>
+				<tr>
+					<td>시설특성</td>
+					<td>{{ facility.fcltychartr }}</td>
+				</tr>
+				<tr>
+					<td>개관연도</td>
+					<td>{{ facility.opende }}</td>
+				</tr>
+				<tr>
+					<td>객석수</td>
+					<td>{{ facility.seatscale }}</td>
+				</tr>
+				<tr>
+					<td>주소</td>
+					<td>{{ facility.adres }}</td>
+				</tr>
+			</table>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'FacilityDetail',
+	props: {
+		facility: {
+			type: Object,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.info-section {
+	h2.prfm-title {
+		color: $primary-lighten-2;
+		margin: 10px 0;
+	}
+}
+.info-table {
+	display: flex;
+	// height: 350px;
+
+	.item-image {
+		width: 300px;
+		padding-top: 10%;
+		background-color: lightgray;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+	}
+	table {
+		flex-grow: 1;
+		// width: 50%;
+		margin-left: 10px;
+
+		tr > td:first-child {
+			width: 100px;
+			padding: 10px 20px 10px 10px;
+			font-weight: bold;
+			border-bottom: 1px solid $gray-scale-2;
+			background-color: $gray-scale-1;
+
+			&:last-child {
+				border-bottom: none;
+				color: red;
+			}
+		}
+		tr > td:last-child {
+			padding: 10px 0px 10px 10px;
+		}
+	}
+}
+</style>
--- src/pages/facility/FacilityDetailPage.vue
+++ src/pages/facility/FacilityDetailPage.vue
@@ -3,44 +3,20 @@
 		<h2>시설 상세 페이지</h2>
 		<loading-spinner v-if="isLoading" />
 		<template v-else>
-			<section class="info-section">
-				<h2 class="prfm-title">
-					{{ facilityData.fcltynm }}
-				</h2>
-				<div class="info-table">
-					<table>
-						<tr>
-							<td>장소</td>
-							<td>{{ facilityData.fcltynm }}</td>
-						</tr>
-						<tr>
-							<td>시설특성</td>
-							<td>{{ facilityData.fcltychartr }}</td>
-						</tr>
-						<tr>
-							<td>개관연도</td>
-							<td>{{ facilityData.opende }}</td>
-						</tr>
-						<tr>
-							<td>객석수</td>
-							<td>{{ facilityData.seatscale }}</td>
-						</tr>
-						<tr>
-							<td>주소</td>
-							<td>{{ facilityData.adres }}</td>
-						</tr>
-					</table>
-				</div>
-			</section>
+			<!-- facility detail -->
+			<facility-detail :facility="facility" />
 			<!-- tabmenu -->
 			<app-tab
 				v-bind:tabs="tabs"
 				:selectedTab="selectedTab"
 				v-on:@tab="onClickTab"
 			/>
-			<div v-if="selectedTab === tabs[0]"></div>
+			<div v-if="selectedTab === tabs[0]">
+				<p>데이터를 표시할 영역입니다.</p>
+			</div>
 			<div v-if="selectedTab === tabs[1]">
 				<!-- if로 그려야 할 것 같은데? -->
+				<p>진행공연 내역을 표시할 영역입니다.</p>
 			</div>
 			<div v-if="selectedTab === tabs[2]">
 				<!-- center 위치 객체를 전달 -->
@@ -54,6 +30,7 @@
 import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import AppTab from '@/components/common/AppTab.vue';
 import AppMap from '@/components/common/AppMap.vue';
+import FacilityDetail from '@/components/facility/FacilityDetail.vue';
 
 import { mapState, mapActions } from 'vuex';
 
@@ -62,6 +39,7 @@ export default {
 		LoadingSpinner,
 		AppTab,
 		AppMap,
+		FacilityDetail,
 	},
 	data() {
 		return {
@@ -69,13 +47,12 @@ export default {
 			mt10id: '',
 			tabs: ['데이터', '진행 공연', '위치'],
 			selectedTab: '',
-			facilityData: [],
 			// daum map 속성
 			center: { lat: '', lng: '' }, // 지도의 중심 좌표
 		};
 	},
 	async created() {
-		await this.getDetail();
+		await this.fetchData();
 		this.selectedTab = this.tabs[0];
 	},
 	computed: {
@@ -83,16 +60,14 @@ export default {
 	},
 	methods: {
 		...mapActions(['fetchFacility']),
-		async getDetail() {
+		async fetchData() {
 			try {
 				this.isLoading = true;
 				// 목록 컴포넌트로부터 아이템의 아이디를 받아서 넘기만 하면 됨
 				this.mt10id = this.$route.params.mt10id;
-				// this.facilityData = data;
 				await this.fetchFacility(this.mt10id);
 				this.isLoading = false;
-				this.center = { lat: this.facilityData.la, lng: this.facilityData.lo };
-				// this.setCenter(this.facilityData.la, this.facilityData.lo);
+				this.center = { lat: this.facility.la, lng: this.facility.lo };
 			} catch (error) {
 				console.error(error);
 			}
@@ -103,62 +78,3 @@ export default {
 	},
 };
 </script>
-
-<style lang="scss" scoped>
-.info-section {
-	h2.prfm-title {
-		color: $primary-lighten-2;
-		margin: 10px 0;
-	}
-}
-
-.info-table {
-	display: flex;
-	// height: 350px;
-
-	.item-image {
-		width: 300px;
-		padding-top: 10%;
-		background-color: lightgray;
-		background-repeat: no-repeat;
-		background-position: center;
-		background-size: cover;
-	}
-	table {
-		flex-grow: 1;
-		// width: 50%;
-		margin-left: 10px;
-
-		tr > td:first-child {
-			width: 100px;
-			padding: 10px 20px 10px 10px;
-			font-weight: bold;
-			border-bottom: 1px solid $gray-scale-2;
-			background-color: $gray-scale-1;
-
-			&:last-child {
-				border-bottom: none;
-				color: red;
-			}
-		}
-		tr > td:last-child {
-			padding: 10px 0px 10px 10px;
-		}
-	}
-}
-.map-table {
-	border: 4px solid $primary-lighten-2;
-	td {
-		display: inline-block;
-		margin: 0 5px;
-	}
-	input {
-		color: red;
-		background: red;
-	}
-	.map-label {
-		font-size: $font-size-md;
-		font-weight: bold;
-	}
-}
-</style>
