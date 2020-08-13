0bac6a3b4e57395c405bc1ae3296ab53f81499f6
imseongtae <dev.haemil@gmail.com>
2020-08-14 04:28:05 +0900

    refactor: Rename facility API name

--- src/api/facility.js
+++ src/api/facility.js
@@ -1,12 +1,13 @@
 import api from '@/api';
 
-function getAllFacilities() {
+// 시설 목록을 조회하는 API
+function fetchFacilitiesAPI() {
 	return api.get('/facilities');
 }
 
-// 특정 공연 목록을 조회하는 API
-function getFacilityDetail(item) {
+// 특정 시설을 조회하는 API
+function fetchFacilityAPI(item) {
 	return api.get(`/facilities/${item}`);
 }
 
-export { getAllFacilities, getFacilityDetail };
+export { fetchFacilitiesAPI, fetchFacilityAPI };
