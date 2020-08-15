15dd82c85c7007e7629de9166a424f7456aa5987
imseongtae <dev.haemil@gmail.com>
2020-08-15 20:59:45 +0900

    fix: Update performance routes to include beforeEnter for 404 error

--- src/routes/performance.js
+++ src/routes/performance.js
@@ -1,3 +1,6 @@
+// store를 사용해 상세보기 페이지로 이동할 때 데이터 요청 실패에 대응하는 error 처리를 진행해보자
+import store from '@/store';
+
 const PERFORMANCE_ROUTES = [
 	{
 		path: '/performances',
@@ -14,6 +17,23 @@ const PERFORMANCE_ROUTES = [
 		},
 		props: {
 			contents: true,
+		},
+		beforeEnter(to, from, next) {
+			// fetchData
+			store
+				.dispatch('fetchPerformance', to.params.mt20id)
+				.then(() => {
+					const performance = store.state.performance;
+					const isFetch = performance.mt20id === to.params.mt20id;
+					if (isFetch) {
+						next();
+					}
+				})
+				.catch(err => {
+					console.log('API 오류로 인한 error가 발생');
+					alert(err.response.data.msg);
+					next(from);
+				});
 		},
 	},
 ];
